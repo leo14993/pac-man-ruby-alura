98d68f2bff6ac8c35c1699c63f58cce265e89d2a
Leonardo P. Rodrigues <leonardolp_rj@hotmail.com>
2020-08-21 21:05:21 -0300

    mudança para meu codigo

--- fogefoge.rb
+++ fogefoge.rb
@@ -1,65 +1,108 @@
 require_relative 'ui'
 
-def le_mapa (numero)
-   arquivo = "mapa#{numero}.txt"
-   text = File.read arquivo
-   mapa = text.split "\n"
+def le_mapa(numero)
+    arquivo = "mapa#{numero}.txt"
+    texto = File.read(arquivo)
+    mapa = texto.split("\n")
 end
 
-def encontre_o_jogador (mapa)
-   caractere_do_heroi = "H"
-   mapa.each_with_index do |linha_atual, linha|
-      coluna_do_heroi = linha_atual.index caractere_do_heroi
-      if coluna_do_heroi
-      return [linha, coluna_do_heroi]
-      end  
-   end
-end
 
-def calcular_nova_posicao (heroi, direcao)
-    heroi = heroi.dup
-    case direcao
-         when "W"
-         heroi [0] -= 1
-         when "S"
-         heroi [0] += 1
-         when "A"
-         heroi [1] -= 1
-         when "D"
-         heroi [1] += 1
+
+def encontra_jogador(mapa)
+    caracter_do_heroi = "H"
+    # mapa.each do |linha_atual|
+    mapa.each_with_index do |linha_atual, linha|
+        coluna_do_heroi = linha_atual.index caracter_do_heroi
+        if coluna_do_heroi
+            return [linha, coluna_do_heroi]    
+            # achei!
+        end
     end
+    # não achei!
+end
+
+def calcula_nova_posicao (heroi, direcao)
+    heroi = heroi.dup
+    movimentos = {
+        "W" => [-1, 0],
+        "S" => [+1, 0],
+        "A" => [0, -1],
+        "D" => [0, +1],
+    }
+    movimento = movimentos[direcao]
+    heroi[0] += movimento[0]
+    heroi[1] += movimento[1]
     heroi
 end
 
-def joga (nome)
-   mapa = le_mapa 1
 
-   while true
-      desenha mapa
-      direcao = pede_movimento
-      heroi = encontre_o_jogador mapa
-      nova_posicao = calcular_nova_posicao heroi, direcao
-    if nova_posicao[0] < 0
+# antes
+# def posicao_valida? mapa, nova_posicao
+
+# 0.1
+# --------------------
+#     if nova_posicao[0] < 0
+#         next
+#     end
+#     if nova_posicao[1] < 0 
+#         next
+#     end
+#     if nova_posicao[0] >= mapa.size
+#         next
+#     end
+#     if nova_posicao[1] >= mapa[0].size
+#         next
+#     end
+# ----------------------
+# 0.2
+# ------------------------
+    # if nova_posicao[0] < 0 || nova_posicao[0] >= mapa.size    
+    # if nova_posicao[1] < 0 || nova_posicao[1] >= mapa[0].size
+# -------------------------
+
+
+#     if mapa[nova_posicao[0]] [nova_posicao[1]] =="X"
+#         next
+#     end
+    
+# end
+
+def posicao_valida? mapa, posicao
+    linhas = mapa.size
+    colunas = mapa[0].size
+    estourou_linhas = posicao[0] < 0 || posicao[0] >= linhas
+    estourou_colunas = posicao[1] < 0 || posicao[1] >= colunas
+    
+    if estourou_linhas || estourou_colunas
+        return false
+    end
+    if mapa[posicao[0]] [posicao[1]] =="X"
+        return false
+    end
+    true
+end
+
+
+def joga(nome)
+    mapa = le_mapa 1
+    
+    while true
+        desenha mapa
+        direcao = pede_movimento
+        heroi = encontra_jogador mapa
+        
+        nova_posicao = calcula_nova_posicao heroi, direcao
+
+        if mapa[nova_posicao[0]] [nova_posicao[1]] == "X"
             next
         end
-        if nova_posicao[1] < 0
-            next
-        end
-        if nova_posicao[0] >= mapa.size
-            next
-        end
-        if nova_posicao[1] >= mapa[0].size
-            next
-        end
-        if mapa[nova_posicao[0]][nova_posicao[1]] == "X"
-            next
-        end
-      mapa[heroi[0]][heroi[1]] = " "
-      mapa[nova_posicao[0]][nova_posicao[1]] = "H"
-   end
+        mapa[heroi[0]] [heroi[1]] = " "
+        mapa[nova_posicao[0]] [nova_posicao[1]] = "H"
+        # mapa[heroi[0]] [heroi[1]] = " "   
+    end
 end
 
 def inicia_fogefoge
-   nome = da_boas_vindas
-   joga nome
+    nome = da_boas_vindas
+    joga nome
 end

f22e38714bd2464c85d6de87758edbdf4ab62484
Leonardo P. Rodrigues <leonardolp_rj@hotmail.com>
2020-08-21 23:43:26 -0300

    refatoracao funcao posicoes_validas_a_partir_de, movimento dos fantasmas parou de funcionar

--- fogefoge.rb
+++ fogefoge.rb
@@ -54,21 +54,12 @@ end
 
 def posicoes_validas_a_partir_de mapa, novo_mapa, posicao
     posicoes = []
-    baixo = [posicao[0] + 1 , posicao[1]]
-    if posicao_valida?(mapa, baixo) && posicao_valida?(novo_mapa, baixo)
-        posicoes << baixo
-    end
-    direita = [posicao[0] , posicao[1] + 1]
-    if posicao_valida?(mapa, direita) && posicao_valida?(novo_mapa, direita)
-        posicoes << direita
-    end
-    esquerda = [posicao[0] + 1 , posicao[1] - 1]
-    if posicao_valida?(mapa, esquerda) && posicao_valida?(novo_mapa, esquerda)
-        posicoes << esquerda
-    end
-    cima = [posicao[0] - 1 , posicao[1]]
-    if posicao_valida?(mapa, cima) && posicao_valida?(novo_mapa, cima)
-        posicoes << cima
+    movimentos = [[+1, 0], [0, +1], [-1, 0], [0, -1]]
+    movimentos.each do |movimento|
+        nova_posicao = [posicao[0] + movimento[0], posicao[1] + movimento[1]]
+        if posicao_valida?(mapa, nova_posicao) && posicao_valida?(novo_mapa, nova_posicao)
+            posicoes << posicao
+        end
     end
     posicoes
 end
